ad87b66ce3985efd16d5fb0cc02e1aaacaeab677
2ebastiaann <2ebastiaann@gmail.com>
2025-12-07 13:37:21 -0500

    frontend actualizado a http

--- frontend/src/app/proyectoDesarrollo2/environments/environment.prod.ts
+++ frontend/src/app/proyectoDesarrollo2/environments/environment.prod.ts
@@ -2,6 +2,6 @@
 export const environment = {
   production: true,
   API_BASE_URL: 'http://ecobahia.eleueleo.com/api',
-  PROF_API_BASE_URL: 'http://apirecoleccion.gonzaloandreslucio.com/api',
+  PROF_API_BASE_URL: 'https://apirecoleccion.gonzaloandreslucio.com/api',
   PERFIL_ID: 'c11bebca-c05b-4a58-afe0-cf280b686365'
 };
--- frontend/src/app/proyectoDesarrollo2/environments/environment.ts
+++ frontend/src/app/proyectoDesarrollo2/environments/environment.ts
@@ -2,6 +2,6 @@
 export const environment = {
   production: false,
   API_BASE_URL: 'http://localhost:3007',
-  PROF_API_BASE_URL: 'http://apirecoleccion.gonzaloandreslucio.com/api',
+  PROF_API_BASE_URL: 'https://apirecoleccion.gonzaloandreslucio.com/api',
   PERFIL_ID: 'c11bebca-c05b-4a58-afe0-cf280b686365'
 };
